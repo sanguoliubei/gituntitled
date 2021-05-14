b04581c345d535fccb5c5eed458b9aca86603d9a
XiaoLu <XiaoLu@163.com>
2021-05-14 22:17:02 +0800

    first commit

---
+++ src/main/java/com/xiaolu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.xiaolu.git;
+
+/**
+ * @author XiaoLu  XiaoLu@163.com
+ * 2021-05-14 21:57
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
